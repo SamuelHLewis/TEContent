8b10be9d219cb7378573b86a1af9d1772fac3d2b
SamuelHLewis <samuel.h.lewis@gmail.com>
2016-12-09 17:04:36 +0000

    Multiple genome/TE input and output plot added

--- TEContent.R
+++ TEContent.R
@@ -2,6 +2,7 @@
 
 suppressPackageStartupMessages(library("argparse"))
 suppressPackageStartupMessages(library("Biostrings"))
+suppressPackageStartupMessages(library("ggplot2"))
 
 TEContent <- function(GenomeFile,TEFile) {
 	sprintf("Input genome file = %s", GenomeFile)
@@ -17,9 +18,7 @@ TEContent <- function(GenomeFile,TEFile) {
 	TErel = (TESize/GenomeSize)*100
 	sprintf("TE content (%%) = %f",TErel)
  	# format output
-	titles=c("Genome size (bp)","TE content (bp)","TE content (%)")
-	stats=c(GenomeSize,TESize,TErel)
-	output=rbind(titles,stats)
+	output=c(GenomeSize,TESize,TErel)
 	return(output)
 }
 
@@ -30,18 +29,48 @@ TEContent <- function(GenomeFile,TEFile) {
 # create parser object
 parser <- ArgumentParser()
 # parse input genome filename
-parser$add_argument("-f", "--genomefile", type="character", help="Genome fasta file")
+parser$add_argument("-f", "--genomefiles", type="character", help="Genome fasta file")
 # parse input TE filename
-parser$add_argument("-t", "--tefile", type="character", help="Transposable element fasta file")
+parser$add_argument("-t", "--tefiles", type="character", help="Transposable element fasta file")
 # collect arguments
 args <- parser$parse_args()
 # assign arguments to variables
-GenomeFile = args$genomefile
-TEFile = args$tefile
+GenomeFiles = strsplit(args$genomefiles,",")[[1]]
+TEFiles = strsplit(args$tefiles,",")[[1]]
+# check that the same number of Genome files and TE files have been given
+if (length(GenomeFiles)==length(TEFiles)) {
+	sprintf("Each genome file has an accompanying TE file - looking good")
+} else {
+	stop("ERROR: there are different numbers of genome and TE files")
+}
 
 ####################
 # function calling #
 ####################
 
-TEContent(GenomeFile,TEFile)
+# empty vectors to hold results for each genome
+Genomes=NULL
+GenomeSizes=NULL
+TESizes=NULL
+TEProps=NULL
 
+# TEContent called on each genome-TE file pair, and results sorted into separate vectors
+for (i in 1:length(GenomeFiles)){
+	cat("Calculating TE content for ",GenomeFiles[i]," & ",TEFiles[i],"\n",sep="")
+	results=TEContent(GenomeFiles[i],TEFiles[i])
+	Genomes = append(Genomes,GenomeFiles[i])
+	GenomeSizes = append(GenomeSizes,as.integer(results[[1]]))
+	TESizes = append(TESizes,as.integer(results[[2]]))
+	TEProps = append(TEProps,as.double(results[[3]]))
+}
+# results reformatted to matrix
+Output=as.data.frame(cbind(Genomes,GenomeSizes,TESizes,TEProps))
+print(Output)
+
+# plot genome size vs TE proportion
+plot=ggplot(data=Output)
+plot=plot+theme_bw()
+plot=plot+geom_point(aes(x=GenomeSizes,y=TEProps,colour=Genomes),size=4)
+pdf(file = "TEContentplot.pdf",width=10.5,height=6.75)
+plot
+dev.off()
