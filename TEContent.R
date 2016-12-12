8896bc781a121189659dd5e8958882b313063286
SamuelHLewis <samuel.h.lewis@gmail.com>
2016-12-12 16:11:16 +0000

    Species argument added

--- TEContent.R
+++ TEContent.R
@@ -23,6 +23,8 @@ TEContent <- function(GenomeFile,TEFile) {
 
 # create parser object
 parser <- ArgumentParser()
+# parse species name
+parser$add_argument("-s", "--species", type="character", help="Genome fasta file")
 # parse input genome filename
 parser$add_argument("-f", "--genomefiles", type="character", help="Genome fasta file")
 # parse input TE filename
@@ -30,13 +32,14 @@ parser$add_argument("-t", "--tefiles", type="character", help="Transposable elem
 # collect arguments
 args <- parser$parse_args()
 # assign arguments to variables
+SpeciesNames = strsplit(args$species,",")[[1]]
 GenomeFiles = strsplit(args$genomefiles,",")[[1]]
 TEFiles = strsplit(args$tefiles,",")[[1]]
-# check that the same number of Genome files and TE files have been given
-if (length(GenomeFiles)==length(TEFiles)) {
-	sprintf("Each genome file has an accompanying TE file - looking good")
+# check that the same number of species names, genome files and TE files have been given
+if (length(SpeciesNames)==length(GenomeFiles) && length(SpeciesNames)==length(TEFiles) && length(GenomeFiles)==length(TEFiles)) {
+	sprintf("Each species has an accompanying genome file & TE file - looking good")
 } else {
-	stop("ERROR: there are different numbers of genome and TE files")
+	stop("ERROR: there are different numbers of species, genome files and TE files")
 }
 
 ####################
@@ -44,6 +47,7 @@ if (length(GenomeFiles)==length(TEFiles)) {
 ####################
 
 # empty vectors to hold results for each genome
+Species=character()
 Genomes=character()
 GenomeSizes=integer()
 TESizes=integer()
@@ -53,16 +57,21 @@ TEProps=numeric()
 for (i in 1:length(GenomeFiles)){
 	cat("Calculating TE content for ",GenomeFiles[i]," & ",TEFiles[i],"\n",sep="")
 	results=TEContent(GenomeFiles[i],TEFiles[i])
+	Species = append(Species,SpeciesNames[i])
 	Genomes = append(Genomes,GenomeFiles[i])
 	GenomeSizes = append(GenomeSizes,as.integer(results[[1]]))
 	TESizes = append(TESizes,as.integer(results[[2]]))
 	TEProps = append(TEProps,as.double(results[[3]]))
 }
 
+# results reformatted to matrix (for screen output only)
+Output=as.data.frame(cbind(Species,Genomes,GenomeSizes,TESizes,TEProps))
+print(Output)
+
 # plot genome size vs TE proportion
 plot=ggplot()
 plot=plot+theme_bw()
-plot=plot+geom_point(aes(x=GenomeSizes,y=TEProps,colour=Genomes),size=4)
+plot=plot+geom_point(aes(x=GenomeSizes,y=TEProps,colour=Species),size=4)
 pdf(file = "TEContentplot.pdf",width=10.5,height=6.75)
 plot
 dev.off()
